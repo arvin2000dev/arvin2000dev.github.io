e9c49fe0a3f57ec6f028b49c119c8b7e52578293
Arvin <atriarvin1@gmail.com>
2022-12-07 14:03:51 +0330

    init #2

--- main.js
+++ main.js
@@ -1,83 +1,262 @@
-/* Please ❤ this if you like it! */
+var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+var ff = navigator.userAgent.indexOf("Firefox") > 0;
+var tap =
+  "ontouchstart" in window || navigator.msMaxTouchPoints
+    ? "touchstart"
+    : "mousedown";
+if (iOS) document.body.classList.add("iOS");
 
-(function ($) {
-  "use strict";
+var fireworks = (function () {
+  var getFontSize = function () {
+    return parseFloat(getComputedStyle(document.documentElement).fontSize);
+  };
 
-  //Page cursors
+  var canvas = document.querySelector(".fireworks");
+  var ctx = canvas.getContext("2d");
+  var numberOfParticules = 24;
+  var distance = 200;
+  var x = 0;
+  var y = 0;
+  var animations = [];
 
-  document
-    .getElementsByTagName("body")[0]
-    .addEventListener("mousemove", function (n) {
-      (t.style.left = n.clientX + "px"),
-        (t.style.top = n.clientY + "px"),
-        (e.style.left = n.clientX + "px"),
-        (e.style.top = n.clientY + "px"),
-        (i.style.left = n.clientX + "px"),
-        (i.style.top = n.clientY + "px");
+  var setCanvasSize = function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  };
+
+  var updateCoords = function (e) {
+    x = e.clientX || e.touches[0].clientX;
+    y = e.clientY || e.touches[0].clientY;
+  };
+
+  var colors = ["#FF324A", "#31FFA6", "#206EFF", "#FFFF99"];
+
+  var createCircle = function (x, y) {
+    var p = {};
+    p.x = x;
+    p.y = y;
+    p.color = colors[anime.random(0, colors.length - 1)];
+    p.color = "#FFF";
+    p.radius = 0;
+    p.alpha = 1;
+    p.lineWidth = 6;
+    p.draw = function () {
+      ctx.globalAlpha = p.alpha;
+      ctx.beginPath();
+      ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+      ctx.lineWidth = p.lineWidth;
+      ctx.strokeStyle = p.color;
+      ctx.stroke();
+      ctx.globalAlpha = 1;
+    };
+    return p;
+  };
+
+  var createParticule = function (x, y) {
+    var p = {};
+    p.x = x;
+    p.y = y;
+    p.color = colors[anime.random(0, colors.length - 1)];
+    p.radius = anime.random(getFontSize(), getFontSize() * 2);
+    p.draw = function () {
+      ctx.beginPath();
+      ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+      ctx.fillStyle = p.color;
+      ctx.fill();
+    };
+    return p;
+  };
+
+  var createParticles = function (x, y) {
+    var particules = [];
+    for (var i = 0; i < numberOfParticules; i++) {
+      var p = createParticule(x, y);
+      particules.push(p);
+    }
+    return particules;
+  };
+
+  var removeAnimation = function (animation) {
+    var index = animations.indexOf(animation);
+    if (index > -1) animations.splice(index, 1);
+  };
+
+  var animateParticules = function (x, y) {
+    setCanvasSize();
+    var particules = createParticles(x, y);
+    var circle = createCircle(x, y);
+    var particulesAnimation = anime({
+      targets: particules,
+      x: function (p) {
+        return p.x + anime.random(-distance, distance);
+      },
+      y: function (p) {
+        return p.y + anime.random(-distance, distance);
+      },
+      radius: 0,
+      duration: function () {
+        return anime.random(1200, 1800);
+      },
+      easing: "easeOutExpo",
+      complete: removeAnimation,
     });
-  var t = document.getElementById("cursor"),
-    e = document.getElementById("cursor2"),
-    i = document.getElementById("cursor3");
-  function n(t) {
-    e.classList.add("hover"), i.classList.add("hover");
-  }
-  function s(t) {
-    e.classList.remove("hover"), i.classList.remove("hover");
-  }
-  s();
-  for (
-    var r = document.querySelectorAll(".hover-target"), a = r.length - 1;
-    a >= 0;
-    a--
-  ) {
-    o(r[a]);
-  }
-  function o(t) {
-    t.addEventListener("mouseover", n), t.addEventListener("mouseout", s);
-  }
+    var circleAnimation = anime({
+      targets: circle,
+      radius: function () {
+        return anime.random(getFontSize() * 8.75, getFontSize() * 11.25);
+      },
+      lineWidth: 0,
+      alpha: {
+        value: 0,
+        easing: "linear",
+        duration: function () {
+          return anime.random(400, 600);
+        },
+      },
+      duration: function () {
+        return anime.random(1200, 1800);
+      },
+      easing: "easeOutExpo",
+      complete: removeAnimation,
+    });
+    animations.push(particulesAnimation);
+    animations.push(circleAnimation);
+  };
 
-  //About page
-
-  $(".about-text").on("click", function () {
-    $("body").addClass("about-on");
-  });
-  $(".about-close").on("click", function () {
-    $("body").removeClass("about-on");
-  });
-
-  //Contact page
-
-  $(".contact-text").on("click", function () {
-    $("body").addClass("contact-on");
-  });
-  $(".contact-close").on("click", function () {
-    $("body").removeClass("contact-on");
+  var mainLoop = anime({
+    duration: Infinity,
+    update: function () {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      animations.forEach(function (anim) {
+        anim.animatables.forEach(function (animatable) {
+          animatable.target.draw();
+        });
+      });
+    },
   });
 
-  //Travel portfolio page
+  document.addEventListener(
+    tap,
+    function (e) {
+      updateCoords(e);
+      animateParticules(x, y);
+    },
+    false
+  );
 
-  $(".travel").on("click", function () {
-    $("body").addClass("travel-on");
-  });
-  $(".travel-close").on("click", function () {
-    $("body").removeClass("travel-on");
+  window.addEventListener("resize", setCanvasSize, false);
+
+  return {
+    boom: animateParticules,
+  };
+})();
+
+var logoAnimation = function () {
+  document.body.classList.add("ready");
+
+  var setDashoffset = function (el) {
+    var l = el.getTotalLength();
+    el.setAttribute("stroke-dasharray", l);
+    return [l, 0];
+  };
+
+  var letters = anime({
+    targets: "#lines path",
+    strokeDashoffset: {
+      value: setDashoffset,
+      duration: 700,
+      easing: "easeOutQuad",
+    },
+    transform: ["translate(0 128)", "translate(0 0)"],
+    delay: function (el, i) {
+      return 750 + i * 120;
+    },
+    duration: 1400,
   });
 
-  //Wildlife portfolio page
+  var dotJSRoll = anime({
+    targets: "#dot-js",
+    transform: ["translate(0 0)", "translate(544 0)"],
+    delay: letters.duration - 800,
+    duration: 800,
+    elasticity: 300,
+  });
 
-  $(".wildlife").on("click", function () {
-    $("body").addClass("wildlife-on");
-  });
-  $(".wildlife-close").on("click", function () {
-    $("body").removeClass("wildlife-on");
+  var dotJSDown = anime({
+    targets: "#dot-js",
+    transform: ["translate(0 -304)", "translate(0 0)"],
+    duration: 500,
+    elasticity: 600,
+    autoplay: false,
   });
 
-  //Nature portfolio page
+  var dotJSUp = anime({
+    targets: "#dot-js",
+    transform: ["translate(0 0) scale(1 3)", "translate(0 -352) scale(1 1)"],
+    duration: 800,
+    easing: "easeOutCirc",
+    complete: dotJSDown.play,
+  });
 
-  $(".nature").on("click", function () {
-    $("body").addClass("nature-on");
+  var boom = anime({
+    duration: 880,
+    complete: function (a) {
+      var dot = dotJSDown.animatables[0].target.getBoundingClientRect();
+      var pos = { x: dot.left + dot.width / 2, y: dot.top + dot.height / 2 };
+      fireworks.boom(pos.x, pos.y);
+    },
   });
-  $(".nature-close").on("click", function () {
-    $("body").removeClass("nature-on");
+
+  var letterI = anime({
+    targets: "#line-i-1",
+    strokeDashoffset: {
+      value: setDashoffset,
+      duration: 700,
+      easing: "easeOutQuad",
+    },
+    transform: function () {
+      return ff
+        ? ["rotate(360)", "rotate(0)"]
+        : ["rotate(360 240 64)", "rotate(0 240 64)"];
+    },
+    duration: 2500,
+    delay: letters.duration - 780,
   });
-})(jQuery);
+
+  var dotI = anime({
+    targets: "#dot-i",
+    transform: ["translate(0 -352) scale(1 3)", "translate(0 0) scale(1 1)"],
+    opacity: {
+      value: [0, 1],
+      easing: "linear",
+      duration: 100,
+    },
+    delay: letters.duration + 250,
+  });
+
+  var JSletters = anime({
+    targets: ["#line-j", "#line-s"],
+    strokeDashoffset: setDashoffset,
+    duration: 1400,
+    delay: function (el, i) {
+      return letterI.duration - 1400 + i * 60;
+    },
+    easing: "easeInOutQuart",
+  });
+
+  var gradients = anime({
+    targets: "#fills *:not(#dot-i)",
+    opacity: [0, 1],
+    delay: letterI.duration - 300,
+    delay: function (el, i, l) {
+      var mid = l / 2;
+      var index = i - mid > mid ? 0 : i;
+      var delay = Math.abs(index - mid);
+      return letterI.duration - 1300 + delay * 30;
+    },
+    duration: 500,
+    easing: "linear",
+  });
+};
+
+document.addEventListener("DOMContentLoaded", logoAnimation, false);
